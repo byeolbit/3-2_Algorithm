6cd8ec4ba125737a84aabf15358866916e440507
byeolbit <info@byeolbit.com>
2016-09-19 17:30:37 +0900

    Initial commit

---
+++ Insertion&Merge/MergeSort.c
@@ -0,0 +1,31 @@
+//
+//  MergeSort.c
+//
+//  Used compiler : Clang-703.0.31
+//
+//  Created by Sang Gyeong Jo on 2016. 9. 19..
+//  Copyright © 2016년 Sang Gyeong Jo. All rights reserved.
+//
+
+#include "Essentials.h"
+
+int main(void){
+    
+    //Init variables;
+    int *data_set;
+    char *file_path = "data02.txt";
+    char *result_file = "hw02_00_201102513_merge.txt";
+    
+    data_set = get_original_data(file_path);
+    
+    set_timer();
+    
+    merge_sort(data_set, data_size);
+    
+    stop_timer();
+    
+    write_sorted_data(data_set, result_file, data_size);
+    
+    calculate_time();
+    
+}
