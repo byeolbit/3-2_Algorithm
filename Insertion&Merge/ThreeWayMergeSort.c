4b516ed4c5049b0a37e40c331b693a17c070fa1f
Sang-Gyueong Jo <info@byeolbit.com>
2016-09-20 09:17:12 +0900

    Init

---
+++ Insertion&Merge/ThreeWayMergeSort.c
@@ -0,0 +1,31 @@
+//
+//  ThreeWayMergeSort.c
+//
+//  Used compiler : Clang-703.0.31
+//
+//  Created by Sang Gyeong Jo on 2016. 9. 20..
+//  Copyright © 2016년 Sang Gyeong Jo. All rights reserved.
+//
+
+#include "Essentials.h"
+
+int main(void){
+    
+    //Init variables;
+    int *data_set;
+    char *file_path = "data02.txt";
+    char *result_file = "hw02_00_201102513_3way_merge.txt";
+    
+    data_set = get_original_data(file_path);
+    
+    set_timer();
+    
+    data_set = three_way_merge_sort(data_set, data_size);
+    
+    stop_timer();
+    
+    write_sorted_data(data_set, result_file, data_size);
+    
+    calculate_time();
+    
+}.
