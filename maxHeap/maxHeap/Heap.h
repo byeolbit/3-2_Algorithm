c2fdea2282a2751830cb2595c8b5601deb4963b3
byeolbit <info@byeolbit.com>
2016-09-28 12:54:12 +0900

    Implement max_heapify, build_heap_data, build_max_heap

--- maxHeap/maxHeap/Heap.h
+++ maxHeap/maxHeap/Heap.h
@@ -17,7 +17,6 @@
 #define LEFT_CHILD(i)   i * 2       //get node left-child
 #define RIGHT_CHILD(i)  i * 2 + 1   //get node right-child
 
-
 struct node {
     int key;
     char *value;
@@ -25,18 +24,19 @@ struct node {
 
 struct priority_queue {
     int size;
-    struct node *array;
+    struct node *heap;
 };
 
 
-void max_heapify(struct priority_queue p_queue, int i);
-void build_max_heap(struct priority_queue p_queue);
+void max_heapify(struct priority_queue p_queue, int i);         //Sort nodes of heap
+void build_max_heap(struct priority_queue p_queue);             //Make heap to max-heap
+struct priority_queue build_heap_data(FILE *fp);                //Get heap data from file
 
-int *insert(int *heap, struct node x);      //Insert x to heap
-int *max(int *heap);                        //Find max element in heap
-int *extract(int *heap);                    //Remove max element in heap
-int increase_key(int *heap, int x, int k);  //Increase value of x to k
-int h_delete(int *heap, int x);             //Delete element x
+int insert(struct priority_queue p_queue, struct node x);       //Insert x to heap
+int *max(struct priority_queue p_queue);                        //Find max element in heap
+int *extract(struct priority_queue p_queue);                    //Remove max element in heap
+int increase_key(struct priority_queue p_queue, int x, int k);  //Increase value of x to k
+int h_delete(struct priority_queue p_queue, int x);             //Delete element x
 
 void max_heapify(struct priority_queue p_queue, int i){
     
@@ -44,27 +44,57 @@ void max_heapify(struct priority_queue p_queue, int i){
     int R = RIGHT_CHILD(i);
     int largest;
     
-    
-    if(L <= p_queue.size && p_queue.array[L].key > p_queue.array[i].key) largest = L;
+    if(L <= p_queue.size && p_queue.heap[L].key > p_queue.heap[i].key) largest = L;
     else largest = i;
     
-    if(R <= p_queue.size && p_queue.array[R].key > p_queue.array[largest].key) largest = R;
+    if(R <= p_queue.size && p_queue.heap[R].key > p_queue.heap[largest].key) largest = R;
     
     if(largest != i){
-        struct node temp = p_queue.array[i];
-        p_queue.array[i] = p_queue.array[largest];
-        p_queue.array[largest] = temp;
-        
+        struct node temp = p_queue.heap[i];
+        p_queue.heap[i] = p_queue.heap[largest];
+        p_queue.heap[largest] = temp;
         max_heapify(p_queue, largest);
     }
-    
 }
 
 void build_max_heap(struct priority_queue p_queue){
-    int heap_size = p_queue.size;
-    for(int i = p_queue.size/2; i>0 ;){
+    int i = 0;
+    for(i = p_queue.size/2; i>0 ; i--){
         max_heapify(p_queue, i);
     }
+}
+
+struct priority_queue build_heap_data(FILE *fp) {
+    
+    /* Init queue */
+    struct priority_queue new_queue;
+    new_queue.size = 0;
+    
+    while(!feof(fp)){
+        if(new_queue.size == 0){
+            new_queue.heap = calloc(1,sizeof(struct node));
+        } else {
+            struct node *temp;
+            temp = realloc(new_queue.heap, (new_queue.size + 1) * sizeof(struct node));
+            new_queue.heap = temp;
+        }
+        fscanf(fp, "%d, %s",&new_queue.heap[new_queue.size].key, new_queue.heap[new_queue.size].value);
+        new_queue.size++;
+    }
+    
+    return new_queue;
+};
+
+int insert(struct priority_queue p_queue, struct node x){
+    
+    struct node *temp;
+    temp = realloc(p_queue.heap, (p_queue.size + 1) * sizeof(struct node));
+    p_queue.heap[p_queue.size] = x;
+    p_queue.size++;
+    
+    max_heapify(p_queue, p_queue.size);
+    
+    return 1;
 }
 
 
