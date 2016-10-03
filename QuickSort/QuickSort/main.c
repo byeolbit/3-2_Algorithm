c4c24efd9c40ce0448d2e1e6e0418730bdc653ec
byeolbit <info@byeolbit.com>
2016-10-03 23:58:21 +0900

    Change header

--- QuickSort/QuickSort/main.c
+++ QuickSort/QuickSort/main.c
@@ -6,10 +6,11 @@
 //  Copyright © 2016년 Sang Gyeong Jo. All rights reserved.
 //
 
-#include <stdio.h>
+#include "Quick.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    
+    
     return 0;
 }
