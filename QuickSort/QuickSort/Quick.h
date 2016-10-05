b984a7afab59c1db2316eeace86aa11f423df795
byeolbit <info@byeolbit.com>
2016-10-05 22:21:16 +0900

    Implement major functions

--- QuickSort/QuickSort/Quick.h
+++ QuickSort/QuickSort/Quick.h
@@ -12,17 +12,19 @@
 #include "Essential.h"
 
 int *build_dataset(FILE *fp);
-int *partition(int *data_set, int p, int r);
+int partition(int *data_set, int f, int l);
+int randomized_partition(int *data_set, int f, int l);
+int *quick_sort(int *data_set, int f, int k);
 
-
-
-int *build_dataset(FILE *fp){
+int *build_dataset(FILE *fp)
+{
     int *data_set;
     int read_count = 0;
     
     data_set = calloc(1, sizeof(int));
     
-    while(!feof(fp)){
+    while(!feof(fp))
+    {
         int *temp_data_set;
         
         temp_data_set = realloc(data_set, (++read_count)*sizeof(int));
@@ -35,10 +37,40 @@ int *build_dataset(FILE *fp){
     return data_set;
 }
 
-int *partition(int *data_set, int p, int r){
+int partition(int *data_set, int f, int l)
+{
+    int x = data_set[l];
+    int i = f-1;
+    int j = f;
     
-    int x = data_set[r];
+    for(j=f; j+1 != i; j++)
+    {
+        if(data_set[j]<=x)
+        {
+            int swap_temp = data_set[++i];
+            data_set[i] = data_set[j];
+            data_set[j] = swap_temp;
+        }
+        int swap_temp = data_set[++i];
+        data_set[i] = data_set[l];
+        data_set[l] = swap_temp;
+    }
+    return i;
+}
+
+int randomized_partition(int *data_set, int f, int l){
     
+    return partition(data_set, f, l);
+}
+
+int *quick_sort(int *data_set, int f, int k)
+{
+    if (f<k)
+    {
+        int q = partition(data_set, f, k);
+        quick_sort(data_set, q, q-1);
+        quick_sort(data_set, q+1, k);
+    }
     return data_set;
 }
 
