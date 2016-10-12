0c1a0dcf870d8517c1405269b5de45c688e6b52c
byeolbit <info@byeolbit.com>
2016-10-12 14:46:55 +0900

    Init functions

--- ClockAngle/ClockAngle/main.c
+++ ClockAngle/ClockAngle/main.c
@@ -8,8 +8,38 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+void run_program();
+void get_input(int *hour, int *min);
+void print_result(int hour, int min, float result_angle);
+
+float calc_angle(int hour, int min);
+float get_difference(float h_angle, float m_angle);
+float get_hour_angle(int hour);
+float get_min_angle(int min);
+
+
+int main(int argc, const char * argv[])
+{
+    run_program();
     return 0;
+}
+
+void get_input(int *hour, int *min)
+{
+}
+
+float calc_angle(int hour, int min)
+{
+    return get_difference(get_hour_angle(hour), get_min_angle(min));
+}
+
+void run_program()
+{
+    while(1)
+    {
+        int hour, min;
+        get_input(&hour, &min);
+        print_result(hour, min, calc_angle(hour, min));
+    }
+    
 }
