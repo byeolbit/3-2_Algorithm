79c8574c78ae838a800774c1daaaa4457ef14d75
Sang-Gyueong Jo <info@byeolbit.com>
2016-10-17 00:15:34 +0900

    Add test for li_plus

--- LargeInteger/LargeInteger/main.c
+++ LargeInteger/LargeInteger/main.c
@@ -9,11 +9,12 @@
 #include "LargeNumber.h"
 
 void test_li_cmp ();
+void test_li_plus ();
 
 int main(int argc, const char * argv[]) {
     
-    test_li_cmp();
-    
+    //test_li_cmp();
+    test_li_plus();
     return 0;
 }
 
@@ -22,5 +23,17 @@ void test_li_cmp ()
     large_int li_test_a = new_li("1231231231231231345315114321344243");
     large_int li_test_b = new_li("1231231231231231345315114371344243");
     
-    printf("%s\n",li_comp(li_test_a, li_test_b).num);
+    printf("%d\n",li_comp(li_test_a, li_test_b));
+    free(li_test_a.num);
+    free(li_test_b.num);
+}
+
+void test_li_plus ()
+{
+    large_int li_test_a = new_li("1010101010101010101010101010101010101010101010101010101010101010101010110101010101010101010101010101010101");
+    large_int li_test_b = new_li("101010101010101010101010101010101010101010101010101010101010101010101011010101010101010101010101010101010");
+    large_int result = li_plus(li_test_a, li_test_b);
+    printf("size_a = %lu, size_b = %lu, result = %lu, %s\n",li_test_a.size,li_test_b.size, result.size, result.num);
+    free(li_test_a.num);
+    free(li_test_b.num);
 }
