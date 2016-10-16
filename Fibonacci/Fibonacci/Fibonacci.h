89ae9679be4c59a437bc1ecb81b19ab55a7516c5
Sang-Gyueong Jo <info@byeolbit.com>
2016-10-17 02:56:53 +0900

    Change int to large_int

--- Fibonacci/Fibonacci/Fibonacci.h
+++ Fibonacci/Fibonacci/Fibonacci.h
@@ -9,31 +9,46 @@
 #ifndef Fibonacci_h
 #define Fibonacci_h
 
-#include <stdlib.h>
+#include "LargeNumber.h"
 
-int fibo_rec(int n);
-int *fibo_array(int n);
-int *fibo_rec_sqr(int n);
+large_int fibo_rec ( large_int n , large_int li_1, large_int li_2);
+large_int fibo_array ( int n );
+large_int *fibo_rec_sqr ( large_int n );
 
-int fibo_rec(int n)
+large_int fibo_rec ( large_int n , large_int li_1, large_int li_2)
 {
-    if ( n<2 ) return n;
-    return fibo_rec( n-1 ) + fibo_rec( n-2 );
+    if ( li_comp(n, li_2 ) == -1 )
+        return n;
+    
+    return li_plus ( fibo_rec ( li_minus ( n, li_1 ), li_1, li_2 ) ,fibo_rec ( li_minus( n, li_2 ), li_1, li_2 ) );
 }
 
-int *fibo_array(int n)
+large_int fibo_array ( int n )
 {
-    int *fib_arr = calloc(n,sizeof(int));
+    
+    large_int *fib_arr = calloc(n, sizeof(large_int) );
+    
+    //large_int fib_arr[82];
+    
     int i = 0;
-    fib_arr[i] = 0;
-    fib_arr[i++] = 1;
+    fib_arr[i] = new_li("0");
+    fib_arr[++i] = new_li("1");
+    i++;
     
-    while ( i<n ) fib_arr[i++] = fib_arr[i-1] + fib_arr[i];
+    while ( i <= n ){
+        fib_arr[i] = li_plus(fib_arr[i-2], fib_arr[i-1]);
+        i++;
+    }
     
-    return fib_arr;
+    large_int result = fib_arr[n];
+    
+    for ( i = 0 ; i <= n ; i++ )
+        free(fib_arr[i].num);
+    
+    return result;
 }
 
-int *fibo_req_sqr(int n)
+large_int *fibo_req_sqr ( large_int n )
 {
     return 0;
 }
