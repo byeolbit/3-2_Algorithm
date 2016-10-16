88d2dc59c74a55057dc2148895444622e04a9723
Sang-Gyueong Jo <info@byeolbit.com>
2016-10-17 02:57:16 +0900

    Insert test code

--- Fibonacci/Fibonacci/main.c
+++ Fibonacci/Fibonacci/main.c
@@ -6,11 +6,20 @@
 //  Copyright © 2016년 Sang-Gyeong Jo. All rights reserved.
 //
 
-#include <stdio.h>
+#include "LargeNumber.h"
 #include "Fibonacci.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    
+    //large_int li_1 = new_li("1");
+    //large_int li_2 = new_li("2");
+
+    printf("%s\n", fibo_array(900000).num);
+    //printf("%llu\n",fib(50));
+    
+    //free(li_1.num);
+    //free(li_2.num);
+    
     return 0;
 }
