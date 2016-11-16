0856d9a399fdb060a5a3894fdf1f3b53b47b1399
byeolbit <info@byeolbit.com>
2016-11-16 13:42:30 +0900

    Rename

---
+++ Dijkstra/Dijkstra/DijkstraPriorityQueue.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Dijkstra
+//
+//  Created by Sang Gyeong Jo on 2016. 11. 9..
+//  Copyright © 2016년 Sang Gyeong Jo. All rights reserved.
+//
+
+#include "Dijkstra.h"
+
+int main(int argc, const char * argv[]) {
+    build_path();
+    print_queue_set();
+    
+}
