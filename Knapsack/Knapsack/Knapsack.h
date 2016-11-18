aac446bb168bc4ff94fcac5893b1d2a75483661a
byeolbit <info@byeolbit.com>
2016-11-18 18:14:25 +0900

    Implement functions : chk_weight_range, get_knap_weight, get_data

--- Knapsack/Knapsack/Knapsack.h
+++ Knapsack/Knapsack/Knapsack.h
@@ -5,7 +5,6 @@
 //  Created by Sang Gyeong Jo on 2016. 11. 18..
 //  Copyright © 2016년 Sang Gyeong Jo. All rights reserved.
 //
-//  System will do memory management
 //
 
 #ifndef Knapsack_h
@@ -15,7 +14,7 @@
 
 #define MINWEIGHT 1     //Min weight of knapsack
 #define MAXWEIGHT 50    //Max weight of knapsack
-#define MAXITEM 2000    //Max number of item
+#define MAXITEM 10000    //Max number of item
 
 int knap_w;     //Weight of knapsack
 int item[MAXITEM][MAXITEM];     //Data of items. Value and Weight
@@ -23,8 +22,39 @@ int OPT_tbl[MAXWEIGHT][MAXITEM];  //OPT table
 
 void get_opt(int i, int w);     //Get OPT table
 void get_best();                //Get best answer from opt table
+void get_knap_weight();         //Get weight of knapback
 void get_data(FILE *fp);        //Get problem data from datafile
 int chk_weight_range(int i);    //Validate weight of knapsack
+
+int chk_weight_range(int i)
+{
+    return i <= MAXWEIGHT && i >= MINWEIGHT;
+}
+
+void get_knap_weight()
+{
+    while(1)
+    {
+        printf("Input the weight of knpasack(1~50) : ");
+        scanf("%d",&knap_w);
+        if(chk_weight_range(knap_w)) break;
+        printf("Wrong range!\n");
+    }
+}
+
+void get_data(FILE *fp)
+{
+    for(int i = 0; !feof(fp); i++)
+        fscanf(fp,"%d,%d,%d\n",&item[i][0],&item[i][0],&item[i][1]);
+    fclose(fp);
+}
+
+void get_opt(int i, int w)
+{
+    int best_w; //best weight in n-weight
+    
+}
+
 
 
 
